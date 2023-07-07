ccc52ea5e113004f2fbe145d5c1d5cc7ec00839a
Andrey Cerqueira <andreycmorais@outlook.com>
2023-07-07 16:49:07 -0300

    Alinhando

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ axios
       let body = document.querySelector("body"); // pega o element `body` e salva numa variavel pra referenciar no JS, note as aspas
       let h5 = document.createElement("h5"); // cria um h5, note as aspas
       h5.classList.add("dados");
-      body.appendChild(h5); // define h5 como filho de body, perceba que h5 nao tem aspas, pq estamos falando da variavel h5, nao de uma string 'h5'
+      container.appendChild(h5); // define h5 como filho de body, perceba que h5 nao tem aspas, pq estamos falando da variavel h5, nao de uma string 'h5'
       let text = document.createTextNode(
         `${response.data[i].today.min}º ${response.data[i].today.max}º ${response.data[i].name}`
       );
